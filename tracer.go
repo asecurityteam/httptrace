4fec7db23a45ac84e863bfcd9a543e3de256d9fa
Kevin Conway <kconway@atlassian.com>
2018-05-14 17:58:20 -0500

    Update log event
    Notable changes include a constructor tracer for non-request scoped
    actions and use of logevent.FromContext for request scoped spans.

---
+++ tracer.go
@@ -0,0 +1,15 @@
+package stridetrace
+
+import (
+	"bitbucket.org/atlassian/logevent"
+	opentracing "github.com/opentracing/opentracing-go"
+	zipkin "github.com/openzipkin/zipkin-go-opentracing"
+)
+
+// NewTracer generates an opentracing.Tracer implementation that uses the given
+// Logger and metadata when generating and emitting spans.
+func NewTracer(logger logevent.Logger, serviceName string, hostPort string) (opentracing.Tracer, error) {
+	var collector = &collector{logger}
+	var recorder = zipkin.NewRecorder(collector, false, hostPort, serviceName)
+	return zipkin.NewTracer(recorder)
+}
